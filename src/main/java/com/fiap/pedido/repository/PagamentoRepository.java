a52177c057db63e64fa798b33ee29d078064f2c2
amour <amouraorr@yahoo.com.br>
2025-07-23 05:19:46 -0300

    Ajuste nesessários.

--- src/main/java/com/fiap/pedido/repository/PagamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fiap.pedido.repository;
-
-import com.fiap.pedido.entity.PadidoPagamentoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoRepository extends JpaRepository<PadidoPagamentoEntity, Long> {
-
-}
